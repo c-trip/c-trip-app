2b09a94fad6ce2a0b15c58c67a8c256335900ca1
Alfredo-capitia <delciiocapitia676@gmail.com>
2026-08-19 00:42:06 +0100

    fix: corrigir import de Link de react-router-dom para react-router
    O projecto usa react-router (v7+), não react-router-dom.
    O import causava erro Vite 'Failed to resolve import'.

--- src/routes/_auth/LoginPage.tsx
+++ src/routes/_auth/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router'
 import PhoneInput from '../../components/PhoneInput'
 
 export default function LoginPage() {
