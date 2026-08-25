6a6b38f5385e0949fca038112c3597a3745d49c2
Alfredo-capitia <delciiocapitia676@gmail.com>
2026-08-25 23:06:29 +0100

    Formato de data alterado para 'hoje, 25 Ago'

--- src/routes/_operator/OperatorDayTrips.tsx
+++ src/routes/_operator/OperatorDayTrips.tsx
@@ -1,35 +1,39 @@
-import { IconMapPin } from '@tabler/icons-react'
+import { IconMapPin , IconClock} from '@tabler/icons-react'
 
 function formatDate(): string {
   const now = new Date()
   const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
-  return `${now.getDate()} ${months[now.getMonth()]} ${now.getFullYear()}`
+  return `hoje, ${now.getDate()} ${months[now.getMonth()]}`
 }
 
 export default function OperatorDayTrips() {
   return (
     <div className="min-h-screen bg-gray-50 font-outfit">
-      <div
-        className="px-5 pt-6 pb-8 rounded-b-3xl"
-        style={{ background: 'linear-gradient(90deg, #2E8B57 0%, #1B7A3D 50%, #0B2F1A 100%)' }}
+      <header
+        className="px-5 pt-10 flex flex-col gap-2 rounded-b-3xl h-[186px]"
+        style={{ background: 'linear-gradient(280deg, #2E8B57 0%, #1B7A3D 40%, #0B2F1A 100%)' }}
       >
         <div className="flex items-center justify-between text-white">
-          <div className="flex items-center gap-2">
-            <IconMapPin className="size-5" />
-            <span className="text-sm font-semibold">Terminal de Viana</span>
+          <div className="flex items-center gap-1">
+            <IconMapPin className="size-4" />
+            <span className="text-[13px] font-semibold">Terminal de Viana</span>
           </div>
-          <span className="text-sm font-medium opacity-90">{formatDate()}</span>
+          <div className='bg-[#FFFFFF33] py-1.5  px-2.5 rounded-4xl flex items-center justify-center 
+          gap-1 border border-[#FFFFFF1F]'>
+         <IconClock className='size-3'/> 
+          <span className="text-xs font-semibold opacity-90">{formatDate()}</span>
+          </div>
+        </div>
+
+      <div className=" mt-2">
+        <h1 className="text-[34px] font-extrabold text-white">Painel do Dia</h1>
+
+        <div className="flex items-end justify-between gap-3 mb-6">
+          <span className="text-sm text-[#FFFFFFB3] whitespace-nowrap">Visão geral em tempo real</span>
+          <div className="h-px w-[100px] bg-[#FFFFFFB3]" />
         </div>
       </div>
-
-      <main className="px-5 -mt-2">
-        <h1 className="text-2xl font-bold text-[#111827] mb-4">Painel do Dia</h1>
-
-        <div className="flex items-center gap-3 mb-6">
-          <span className="text-sm text-gray-500 whitespace-nowrap">Visão geral em tempo real</span>
-          <div className="h-px flex-1 bg-gray-300" />
-        </div>
-      </main>
+      </header>
     </div>
   )
 }
